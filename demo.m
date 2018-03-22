ee05ac65d549fd08ada89ad2ec7665ebdd82fd9d
martinli0822 <martinli0822@gmail.com>
2018-03-22 16:22:34 +0800

    Code for TIP 2018

---
+++ demo.m
@@ -0,0 +1,39 @@
+clear;
+img = double(imread('9.bmp'));
+
+para.epsilon_stop_L = 1e-3;
+para.epsilon_stop_R = 1e-3;
+para.epsilon = 10/255;
+para.u = 1;
+para.ro = 1.5;
+para.lambda = 5;
+para.beta = 0.01;
+para.omega = 0.01;
+para.delta = 10;
+
+gamma = 2.2;
+
+[R, L, N] = lowlight_enhancement(img, para);
+
+res = R.*L.^(1/gamma);
+
+figure,imshow(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
